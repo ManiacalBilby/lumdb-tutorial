1b31920c5188d9e9bc02b8bebf0c6cbb0bb59e4d
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-07-18 11:02:58 -0400

    Installed redux and react-redux. Created rootReducer, reducer, and store.

--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -28,7 +28,7 @@ class MovieDetail extends Component {
 
   render() {
     const { movie } = this.state;
-
+    if (!movie.id) return null;
     return (
       <MovieWrapper backdrop={`${BACKDROP_PATH}${movie.backdrop_path}`}>
         <MovieInfo>
@@ -36,11 +36,6 @@ class MovieDetail extends Component {
             <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
           </Overdrive>
           <div>
-            {this.state.movie.title ? (
-              <h1>Hello</h1>
-            ) : (
-              <h1>Hi</h1>
-            )}
             <h1>{movie.title}</h1>
             <h3>{movie.release_date}</h3>
             <p>{movie.overview}</p>
