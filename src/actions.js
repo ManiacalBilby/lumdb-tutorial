79a0e3b4acf44e49dff27e5532aacc541c38a7ea
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-07-19 17:05:53 -0400

    Installed/imported redux-thunk. Created getMovies function for API call in actions

--- src/actions.js
+++ src/actions.js
@@ -1,7 +1,19 @@
 export const TOGGLE_MESSAGE = 'TOGGLE_MESSAGE';
+export const GET_MOVIES = 'GET_MOVIES';
 
 export function toggleMessage() {
   return {
     type: 'TOGGLE_MESSAGE',
-  }
+  };
+}
+
+export function getMovies() {
+  return async function (dispatch) {
+    const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=3446451d0389ed9021458c5d9cd13047&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+    const movies = await res.json();
+    return dispatch({
+      type: 'GET_MOVIES',
+      data: movies.results,
+    });
+  };
 }
