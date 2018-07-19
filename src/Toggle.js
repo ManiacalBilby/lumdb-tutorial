5beac19f192e589e09ada68487df3a75222d1661
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-07-19 12:13:07 -0400

    Created actions.js and added toggleMessage function. Imported function into Toggle.js

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 
+import { toggleMessage } from './actions'
+
 const Toggle = ({ messageVisibility, dispatch }) => (
   <div>
     {messageVisibility &&
     <p>Texty text text text</p>
         }
-    <button onClick={() => dispatch({
-      type: 'TOGGLE_MESSAGE',
-    })}
-    >Toggle Me
+    <button onClick={() => dispatch(toggleMessage())}>Toggle Me
     </button>
   </div>
 );
